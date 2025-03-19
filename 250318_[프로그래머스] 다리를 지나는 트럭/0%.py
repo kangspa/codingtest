673b792cb73972a49435c92920279efe5df1a43e
kangspa <111270327+kangspa@users.noreply.github.com>
2025-03-19 23:06:48 +0900

    edit:250318-Fail

--- 250318_[프로그래머스] 다리를 지나는 트럭/0%.py
+++ 250318_[프로그래머스] 다리를 지나는 트럭/0%.py
@@ -2,12 +2,31 @@ from collections import deque
 
 def solution(bridge_length, weight, truck_weights):
     answer = 0
-    Q = deque(truck_weights)
-    now_truck, now_count = 0, 0
-    while Q:
-        now_truck = Q.popleft()
-        now_weight += now_truck
-        now_count += now_truck
+    Remain = deque(truck_weights)
+    Bridge = deque()
+    while Remain:
+        tmp = None
+        # Bridge에 트럭을 계속해서 추가해나가는 부분
+        while True:
+            # Bridge[0]의 합이 weight을 넘기면 마지막에 넣은 트럭은 우선 제거해두고 진행
+            if sum(b[0] for b in Bridge) > weight:
+                tmp = Bridge.pop()
+                break
+            # 하중을 넘기지 않았다면, 우선 Bridge 안의 트럭을 1대씩 앞으로 이동
+            for truck in Bridge: truck[1] += 1
+            # 첫번째 truck의 위치가 bridge_length를 넘기면 Bridge에서 제거
+            if Bridge:
+                if Bridge[0][1] > bridge_length: Bridge.popleft()
+            # Bridge에 다음 truck을 추가해준다.
+            if Remain: Bridge.append([Remain.popleft(), 1])
+            answer += 1
+        # Bridge에 더이상 truck이 올라가지 못하는 경우, 첫번째 트럭이 나갈 때까지 한번에 연산
+        _, now = Bridge.popleft()
+        n = (bridge_length+1)-now
+        for truck in Bridge: truck[1] += n
+        answer += n
+        # tmp에 저장해뒀던, 넣었다 뺀 트럭 정보를 다시 입력해준다.
+        Bridge.append(tmp)
     return answer
 
 if __name__=="__main__":
