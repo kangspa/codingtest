2f1cfd3a48529a03184d940d4dfbcbb9676dcba7
kangspa <111270327+kangspa@users.noreply.github.com>
2025-03-21 19:17:21 +0900

    add:250321-init.py

---
+++ 250321_[프로그래머스] 등굣길/0%.py
@@ -0,0 +1,9 @@
+def solution(m, n, puddles):
+    answer = 0
+    return answer
+
+if __name__=="__main__":
+    m, n = 4, 3
+    puddles = [[2, 2]]
+    answer = solution(m, n, puddles)
+    print(f"{answer==4} / 정답 : 4 / 출력 : {answer}")
